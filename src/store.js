b287f1b00ebd86f70148499826adc55c81b436ad
wangkly <wanggang@qianmi.com>
2019-01-25 18:08:34 +0800

    modify

--- src/store.js
+++ src/store.js
@@ -1,7 +1,17 @@
-import {createStore,combineReducers} from 'redux';
+import {createStore,combineReducers,applyMiddleware} from 'redux';
 import HomeReducer from './reducers/home-reduer';
+
+import createSagaMiddleware from 'redux-saga'
+import mySaga from './saga'
+
+const sageMiddleware = createSagaMiddleware(mySaga);
+
 let rootReducer = combineReducers({HomeReducer}) 
 
-let store = createStore(rootReducer)
+let store = createStore(rootReducer,applyMiddleware(sageMiddleware));
+
+sageMiddleware.run(mySaga);
+
+export default store;
 
 
